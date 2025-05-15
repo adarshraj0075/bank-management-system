8d4aa49cf25e1566b646301f64393a63c0541d1f
a2v10 <hey.a2v10@gmail.com>
2025-05-15 15:33:47 +0530

    Refactor database connection to use mysql2 and update package dependencies

--- connection.js
+++ connection.js
@@ -1,15 +1,18 @@
-const mysql = require("mysql")
+// Before: const mysql = require('mysql');
+const mysql = require('mysql2');
+
 const con = mysql.createConnection({
-    host: process.env.DB_HOST || "mysql.railway.internal",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "xsXJusgvZereNTfllxxFTRFKckcgZTmx",
-    database: process.env.DB_NAME || "railway",
-    port: process.env.DB_PORT || 53910
+  host: process.env.DB_HOST     || 'yamabiko.proxy.rlwy.net',
+  port: process.env.DB_PORT     || 53910,
+  user: process.env.DB_USER     || 'root',
+  password: process.env.DB_PASSWORD || 'xsXJusgvZereNTfllxxFTRFKckcgZTmx',
+  database: process.env.DB_NAME || 'railway',
 });
 
-con.connect((err) => {
-    if (err) throw err;
-    console.log("Connection created..!!");
+con.connect(err => {
+  if (err) throw err;
+  console.log("Connection created..!!");
 });
+
 
 module.exports.con = con;
